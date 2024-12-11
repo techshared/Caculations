37dcb6b3dd7d6ae15d389d76378f17fd003e30c7
awscstegithub <awscstegithub@outlook.com>
2024-12-11 10:56:14 +0800

    20241211 - replace class name and method

--- Combination.java
+++ Combination.java
@@ -2,28 +2,35 @@ package cn.abs;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class Combination {
 
     /**
-     * 互不相同，无重复数字的组合
-     * arg[0...(n-1)] 可用于组合的数
-     * arg[n] 组合数的位数
+     * 互不相同，无重复数字的排列
+     * arg[0...(n-1)] 可用于排列的数
+     * arg[n] 排列数的位数
      */
     public static void main(String[] args) {
-        combine(null, new ArrayList<String>(Arrays.asList(args)), 1, 3);
+        combine(null, new ArrayList<String>(Arrays.asList(args)), 1, 4);
     }
 
-    public static void combine(StringBuffer stringBuffer, List<String> list, int count, int exceptCount) {
+    /**
+     * @param stringBuilder 用于组合的拼接
+     * @param list          组合数序列
+     * @param level         目前位数
+     * @param exceptedLevel 组合期待位数
+     */
+    public static void combine(StringBuilder stringBuilder, ArrayList<String> list, int level, int exceptedLevel) {
         for (int i = 0; i < list.size(); i++) {
-            StringBuffer buffer = stringBuffer != null ? new StringBuffer(stringBuffer) : new StringBuffer();
-            buffer.replace(count - 1, count, list.get(i));
-            if (count == exceptCount) {
-                System.out.println(buffer);
+            StringBuilder builder = stringBuilder != null ? new StringBuilder(stringBuilder) : new StringBuilder();
+            builder.replace(level - 1, level, list.get(i));
+            if (level == exceptedLevel) {
+                System.out.println(builder);
                 continue;
             }
-            combine(buffer, list.subList(i + 1, list.size()), count + 1, exceptCount);
+            ArrayList<String> arrayList = new ArrayList<>(list);
+            arrayList.remove(i);
+            combine(builder, arrayList, level + 1, exceptedLevel);
         }
     }
 
--- Permutation.java
+++ Permutation.java
@@ -2,29 +2,34 @@ package cn.abs;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Permutation {
 
     /**
-     * 互不相同，无重复数字的排列
-     * arg[0...(n-1)] 可用于排列的数
-     * arg[n] 排列数的位数
+     * 互不相同，无重复数字的组合
+     * arg[0...(n-1)] 可用于组合的数
+     * arg[n] 组合数的位数
      */
-    public static void main(String[] args) throws NoSuchFieldException, ClassNotFoundException, IllegalAccessException {
+    public static void main(String[] args) {
         arrange(null, new ArrayList<String>(Arrays.asList(args)), 1, 3);
     }
 
-    public static void arrange(StringBuffer stringBuffer, ArrayList<String> list, int count, int exceptCount) {
+    /**
+     * @param stringBuilder 用于组合的拼接
+     * @param list          组合数序列
+     * @param level         目前位数
+     * @param exceptedLevel 组合期待位数
+     */
+    public static void arrange(StringBuilder stringBuilder, List<String> list, int level, int exceptedLevel) {
         for (int i = 0; i < list.size(); i++) {
-            StringBuffer buffer = stringBuffer != null ? new StringBuffer(stringBuffer) : new StringBuffer();
-            buffer.replace(count - 1, count, list.get(i));
-            if (count == exceptCount) {
-                System.out.println(buffer);
+            StringBuilder builder = stringBuilder != null ? new StringBuilder(stringBuilder) : new StringBuilder();
+            builder.replace(level - 1, level, list.get(i));
+            if (level == exceptedLevel) {
+                System.out.println(builder);
                 continue;
             }
-            ArrayList<String> arrayList = new ArrayList<>(list);
-            arrayList.remove(i);
-            arrange(buffer, arrayList, count + 1, exceptCount);
+            arrange(builder, list.subList(i + 1, list.size()), level + 1, exceptedLevel);
         }
     }
 
